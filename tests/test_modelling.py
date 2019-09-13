f86c4135df39689f64367916d0b6961e04fb415c
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2019-09-13 18:39:52 +0200

    Update tests to be more realistic (using random one-hot array as base before changing the array to match a certain class). Use 200 instead of 100 arrays per class when testing. Use absolute value of gradient activations in regression, as opposed to summing them up as we to in classification (because we are as interested in inputs that decrease and increae regression output).

--- tests/test_modelling.py
+++ tests/test_modelling.py
@@ -66,7 +66,7 @@ def create_test_optimizer(create_test_cl_args, create_test_model):
     return optimizer
 
 
-def check_snp_types(cls_name, top_grads_msk, expected_idxs):
+def check_snp_types(cls_name, top_grads_msk, expected_idxs, at_least_n):
     """
     Adds an additional check for SNP types (i.e. reference homozygous, heterozygous,
     alternative homozygous, missing).
@@ -74,21 +74,51 @@ def check_snp_types(cls_name, top_grads_msk, expected_idxs):
     Used when we have masked out the SNPs (otherwise the 0s in the one hot might have
     a high activation, since they're saying the same thing as a 1 being in a spot).
     """
-    top_idxs = list(top_grads_msk[cls_name]["top_n_grads"].argmax(0))
-    assert top_idxs == expected_idxs
+    top_idxs = np.array(top_grads_msk[cls_name]["top_n_grads"].argmax(0))
+    expected_idxs = np.array(expected_idxs)
+
+    if at_least_n == "all":
+        assert (top_idxs == expected_idxs).all()
+    else:
+        assert len(set(top_idxs).intersection(set(expected_idxs))) >= at_least_n
 
 
-def check_identified_snps(arrpath, expected_top_indxs, top_row_grads_dict, check_types):
+def check_identified_snps(
+    arrpath, expected_top_indxs, top_row_grads_dict, check_types, at_least_n="all"
+):
+    """
+    NOTE: We have the `at_least_n` to check for a partial match of found SNPs. Why?
+    Because when doing these tests, we are making the inputs per class the same
+    for multiple spots, in the case of regression this leads the network to only "need"
+    to identify a part of the SNPs to create an output (possibly because we are not
+    using a "correctness criteria" for regression, like we do with classification (i.e.
+    only gather activations for correctly predicted classes).
+
+    :param arrpath: Path to the accumulated grads / activation array.
+    :param expected_top_indxs: Expected SNPs to be identified.
+    :param top_row_grads_dict: What row is expected to be activated per class.
+    :param check_types:  Whether to check the SNP types as well as the SNPs themselves
+    (i.e. homozygous reference, etc).
+    :param at_least_n: At least how many SNPs must be identified to pass the test.
+    :return:
+    """
     top_grads_array = np.load(arrpath, allow_pickle=True)
+
+    # get dict from array
     top_grads_dict: dict = top_grads_array[()]
 
     for cls in top_grads_dict.keys():
-        assert sorted(top_grads_dict[cls]["top_n_idxs"]) == expected_top_indxs
+        actual_top = np.array(sorted(top_grads_dict[cls]["top_n_idxs"]))
+        expected_top = np.array(expected_top_indxs)
+        if at_least_n == "all":
+            assert (actual_top == expected_top).all()
+        else:
+            assert len(set(actual_top).intersection(set(expected_top))) >= at_least_n
 
         expected_top_rows = top_row_grads_dict[cls]
 
         if check_types:
-            check_snp_types(cls, top_grads_dict, expected_top_rows)
+            check_snp_types(cls, top_grads_dict, expected_top_rows, at_least_n)
 
 
 @pytest.mark.parametrize(
@@ -155,7 +185,7 @@ def test_classification_snp_identification(
 
     arrpath = run_path / f"samples/{cl_args.n_epochs}/top_acts.npy"
     arrpath_msk = run_path / f"samples/{cl_args.n_epochs}/top_grads_masked.npy"
-    expected_top_indxs = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+    expected_top_indxs = list(range(50, 1000, 100))
     top_row_grads_dict = {"Asia": [1] * 10, "Europe": [2] * 10, "Africa": [0] * 10}
 
     for path in [arrpath, arrpath_msk]:
@@ -215,16 +245,18 @@ def test_regression(
     df = pd.read_csv(run_path / "training_history.log")
 
     assert df.loc[:, "t_r2"].max() > 0.9
-    assert df.loc[:, "v_r2"].max() > 0.9
+    assert df.loc[:, "v_r2"].max() > 0.8
 
     arrpath = run_path / f"samples/{cl_args.n_epochs}/top_acts.npy"
     arrpath_msk = run_path / f"samples/{cl_args.n_epochs}/top_grads_masked.npy"
-    expected_top_indxs = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+    expected_top_indxs = list(range(50, 1000, 100))
     top_row_grads_dict = {"Regression": [0] * 10}
 
     for path in [arrpath, arrpath_msk]:
         check_types = True if path == arrpath_msk else False
-        check_identified_snps(path, expected_top_indxs, top_row_grads_dict, check_types)
+        check_identified_snps(
+            path, expected_top_indxs, top_row_grads_dict, check_types, 5
+        )
 
     if not keep_outputs:
         cleanup()
