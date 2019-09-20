907c7af74aa1cd162fb367a090384f4ce536161b
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2019-09-20 14:11:10 +0200

    Fix test after updating NA drop in previous commit.

--- tests/test_data_loading_funcs.py
+++ tests/test_data_loading_funcs.py
@@ -9,7 +9,9 @@ def test_make_random_snps_missing():
     test_array = torch.zeros((1, 4, 1000), dtype=torch.uint8)
     test_array[:, 0, :] = 1
 
-    patch_target = "human_origins_supervised.data_load.data_loading_funcs.torch.randint"
+    patch_target = (
+        "human_origins_supervised.data_load.data_loading_funcs.np.random.choice"
+    )
     with patch(patch_target, autospec=True) as mock_target:
         mock_return = torch.tensor([1, 2, 3, 4, 5], dtype=torch.long)
         mock_target.return_value = mock_return
@@ -18,4 +20,5 @@ def test_make_random_snps_missing():
 
         assert (array.sum(1) != 1).sum() == 0
         expected_missing = torch.tensor([1] * 5, dtype=torch.uint8)
+        breakpoint()
         assert (array[:, 3, mock_return] == expected_missing).all()
