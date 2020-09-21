7d8845300af297e15f75aac719b845336fa03747
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2020-09-21 19:41:04 +0200

    Use SplitResidualBlocks in FullySplitMLP model.

--- snp_pred/models/models_split_mlp.py
+++ snp_pred/models/models_split_mlp.py
@@ -7,7 +7,7 @@ from aislib.misc_utils import get_logger
 from aislib.pytorch_modules import Swish
 from torch import nn
 
-from snp_pred.models.layers import SplitLinear, MLPResidualBlock
+from snp_pred.models.layers import SplitLinear, MLPResidualBlock, SplitMLPResidualBlock
 from snp_pred.models.models_base import (
     ModelBase,
     create_multi_task_blocks_with_first_adaptor_block,
@@ -144,7 +144,7 @@ class FullySplitMLPModel(ModelBase):
             dropout_p=self.cl_args.rb_do,
         )
 
-        cur_dim = self.split_blocks[-1][-1].out_features
+        cur_dim = self.split_blocks[-1].out_features
         task_names = tuple(self.num_classes.keys())
         task_resblocks_kwargs = {
             "in_features": self.fc_task_dim,
@@ -157,10 +157,7 @@ class FullySplitMLPModel(ModelBase):
             branch_names=task_names,
             block_constructor=MLPResidualBlock,
             block_constructor_kwargs=task_resblocks_kwargs,
-            first_layer_kwargs_overload={
-                "in_features": cur_dim + self.extra_dim,
-                "full_preactivation": True,
-            },
+            first_layer_kwargs_overload={"in_features": cur_dim + self.extra_dim},
         )
 
         final_act_spec = self.get_final_act_spec(
@@ -280,16 +277,14 @@ def _generate_split_blocks(
 
     block_layer_spec_copy = copy(block_layer_spec)
 
-    first_spec = get_split_extractor_spec(
+    first_block = SplitMLPResidualBlock(
         in_features=in_features,
         split_size=kernel_width,
         out_feature_sets=2 ** channel_exp_base,
         dropout_p=dropout_p,
     )
-    first_block = initialize_modules_from_spec(spec=first_spec)
 
     block_modules = [first_block]
-    block_layer_spec_copy[0] -= 1
 
     for cur_layer_index, block_dim in enumerate(block_layer_spec_copy):
         for block in range(block_dim):
@@ -299,15 +294,14 @@ def _generate_split_blocks(
             while cur_out_feature_sets >= cur_kernel_width:
                 cur_kernel_width *= 2
 
-            cur_size = block_modules[-1][-1].out_features
+            cur_size = block_modules[-1].out_features
 
-            cur_spec = get_split_extractor_spec(
+            cur_block = SplitMLPResidualBlock(
                 in_features=cur_size,
                 split_size=cur_kernel_width,
                 out_feature_sets=cur_out_feature_sets,
                 dropout_p=dropout_p,
             )
-            cur_block = initialize_modules_from_spec(spec=cur_spec)
 
             block_modules.append(cur_block)
 
