45cda254e74570093ecaf69d2fc357597cb99d64
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2020-09-21 20:00:37 +0200

    Bugfix: Remove implicit reduction of block_layer spec due to first block, now that we are passing the size of the feature space after the first block to the split block constructor.

--- snp_pred/models/models_split_mlp.py
+++ snp_pred/models/models_split_mlp.py
@@ -289,7 +289,6 @@ def _generate_split_blocks(
     first_block = initialize_modules_from_spec(spec=first_spec)
 
     block_modules = [first_block]
-    block_layer_spec_copy[0] -= 1
 
     for cur_layer_index, block_dim in enumerate(block_layer_spec_copy):
         for block in range(block_dim):
