abfa44cd1055619c2862a1ec5abc1b56b8c77691
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2021-06-18 14:35:42 +0200

    Add logo to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,11 @@ from datetime import datetime
 """Sphinx configuration."""
 project = "EIR"
 author = "Arnor Sigurdsson"
+html_logo = "source/_static/img/EIR_logo_white.png"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 copyright = f"{datetime.now().year}, {author}"
 html_theme = "sphinx_rtd_theme"
 extensions = ["sphinx_copybutton"]
