4f42406bfc3e60e06653bfc3f9574ee99df62ce4
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2021-06-22 12:23:34 +0200

    Update requirements. More specific python version support (3.8, 3.9). Update requirements{-dev}.sh with more explicit checking of the package itself being installed. Add notes regarding venvs when running tox.

--- misc/update_reqs.sh
+++ misc/update_reqs.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
 poetry export -f requirements.txt --without-hashes --output requirements.txt
-echo "-e ." >> requirements.txt
+if ! grep -F -q "-e ." requirements.txt; then
+  echo "-e ." >> requirements.txt
+fi
 
 poetry export -f requirements.txt --without-hashes --dev --output requirements-dev.txt
-echo "-e ." >> requirements-dev.txt
+if ! grep -F -q "-e ." requirements-dev.txt; then
+  echo "-e ." >> requirements-dev.txt
+fi
