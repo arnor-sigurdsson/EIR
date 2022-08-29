3d6ea3ea988e037337f7cb62ccdf7bda3da14f9c
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2022-08-29 11:25:04 +0200

    Update tests for output setup.

--- tests/test_setup/test_output_setup.py
+++ tests/test_setup/test_output_setup.py
@@ -1,5 +1,15 @@
+from typing import TYPE_CHECKING
+
+import pytest
+
 from eir.data_load import label_setup
-from eir.setup.output_setup import set_up_num_outputs_per_target
+from eir.setup.output_setup import (
+    set_up_num_outputs_per_target,
+    get_output_name_config_iterator,
+)
+
+if TYPE_CHECKING:
+    from eir.setup.config import Configs
 
 
 def test_set_up_num_classes(get_transformer_test_data):
@@ -13,3 +23,60 @@ def test_set_up_num_classes(get_transformer_test_data):
 
     assert num_classes["Height"] == 1
     assert num_classes["Origin"] == 3
+
+
+@pytest.mark.parametrize(
+    "create_test_data",
+    [
+        {"task_type": "binary", "modalities": ["omics", "sequence"]},
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "create_test_config_init_base",
+    [
+        {
+            "injections": {
+                "input_configs": [
+                    {
+                        "input_info": {"input_name": "test_genotype"},
+                        "model_config": {
+                            "model_type": "cnn",
+                            "model_init_config": {"l1": 1e-03},
+                        },
+                    },
+                    {
+                        "input_info": {"input_name": "test_sequence"},
+                    },
+                    {
+                        "input_info": {"input_name": "test_tabular"},
+                        "input_type_info": {
+                            "input_cat_columns": ["OriginExtraCol"],
+                            "input_con_columns": ["ExtraTarget"],
+                        },
+                        "model_config": {"model_type": "tabular"},
+                    },
+                ],
+                "output_configs": [
+                    {
+                        "output_info": {"output_name": "test_output"},
+                        "output_type_info": {
+                            "target_cat_columns": ["Origin"],
+                            "target_con_columns": [],
+                        },
+                    },
+                ],
+            },
+        }
+    ],
+    indirect=True,
+)
+def test_get_input_name_config_iterator(create_test_config: "Configs"):
+    test_configs = create_test_config
+
+    named_output_configs = get_output_name_config_iterator(
+        output_configs=test_configs.output_configs
+    )
+    for name, config in named_output_configs:
+        name_from_config = config.output_info.output_name
+        assert name == name_from_config
