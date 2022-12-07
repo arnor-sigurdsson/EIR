164fadfc578f0c0d591082648c16b352003acf4d
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2022-12-07 10:49:18 +0100

    Add better logging to predict when input/output configs are not matching with the training experiment. Refactor predict slightly.

---
+++ eir/predict_modules/predict_data.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from eir.data_load import datasets
+from eir.data_load.datasets import al_datasets
+from eir.data_load.label_setup import al_label_dict
+from eir.setup.config import Configs
+from eir.setup.input_setup import al_input_objects_as_dict
+from eir.setup.output_setup import al_output_objects_as_dict
+
+
+def set_up_default_dataset(
+    configs: Configs,
+    target_labels_dict: Union[None, al_label_dict],
+    inputs_as_dict: al_input_objects_as_dict,
+    outputs_as_dict: al_output_objects_as_dict,
+) -> al_datasets:
+
+    test_dataset_kwargs = datasets.construct_default_dataset_kwargs_from_cl_args(
+        target_labels_dict=target_labels_dict,
+        outputs=outputs_as_dict,
+        inputs=inputs_as_dict,
+        test_mode=True,
+    )
+
+    test_dataset = datasets.DiskDataset(**test_dataset_kwargs)
+
+    return test_dataset
---
+++ tests/test_predict_modules/test_predict_target_setup.py
@@ -0,0 +1,78 @@
+import pytest
+
+import eir.predict_modules
+from eir import predict, train
+from eir.setup import config
+from tests.conftest import TestDataConfig
+
+
+@pytest.mark.parametrize("create_test_data", [{"task_type": "multi"}], indirect=True)
+@pytest.mark.parametrize(
+    "create_test_config_init_base",
+    [
+        {
+            "injections": {
+                "global_configs": {
+                    "output_folder": "extra_inputs",
+                },
+                "input_configs": [
+                    {
+                        "input_info": {"input_name": "test_genotype"},
+                        "model_config": {"model_type": "linear"},
+                    },
+                    {
+                        "input_info": {"input_name": "test_tabular"},
+                        "input_type_info": {
+                            "input_cat_columns": [],
+                            "input_con_columns": ["ExtraTarget"],
+                        },
+                        "model_config": {"model_type": "tabular"},
+                    },
+                ],
+                "output_configs": [
+                    {
+                        "output_info": {"output_name": "test_output"},
+                        "output_type_info": {
+                            "target_cat_columns": ["Origin"],
+                            "target_con_columns": ["Height"],
+                        },
+                    },
+                ],
+            },
+        },
+    ],
+    indirect=True,
+)
+def test_load_labels_for_predict(
+    create_test_data: TestDataConfig,
+    create_test_config: config.Configs,
+):
+    """
+    Note here we are treating the generated test data (i.e. by tests, not test-set-data)
+    as the testing-set.
+    """
+    test_configs = create_test_config
+
+    test_ids = predict.gather_all_ids_from_output_configs(
+        output_configs=test_configs.output_configs
+    )
+
+    tabular_infos = train.get_tabular_target_file_infos(
+        output_configs=test_configs.output_configs
+    )
+    assert len(tabular_infos) == 1
+    target_tabular_info = tabular_infos["test_output"]
+
+    df_test = eir.predict_modules.predict_target_setup._load_labels_for_predict(
+        tabular_info=target_tabular_info, ids_to_keep=test_ids
+    )
+
+    # make sure that target columns are unchanged (within expected bounds)
+    assert len(target_tabular_info.con_columns) == 1
+    con_target_column = target_tabular_info.con_columns[0]
+    assert df_test[con_target_column].max() < 220
+    assert df_test[con_target_column].min() > 130
+
+    assert len(target_tabular_info.cat_columns) == 1
+    cat_target_column = target_tabular_info.cat_columns[0]
+    assert set(df_test[cat_target_column]) == {"Asia", "Africa", "Europe"}
