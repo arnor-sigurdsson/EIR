8b69eccbbfe240e111bf53e9fb723bc11ef85a5f
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2023-10-12 09:30:06 -0600

    Update RTD configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -4,16 +4,18 @@ from datetime import datetime
 
 sys.path.insert(0, os.path.abspath("../eir"))
 
-"""Sphinx configuration."""
 project = "EIR"
 author = "Arnor Sigurdsson"
+
+copyright = f"{datetime.now().year}, {author}"
+
 html_logo = "source/_static/img/EIR_logo_white.png"
 html_theme_options = {
     "logo_only": True,
     "display_version": False,
 }
-copyright = f"{datetime.now().year}, {author}"
 html_theme = "sphinx_rtd_theme"
+
 extensions = [
     "sphinx.ext.napoleon",
     "sphinx_copybutton",
