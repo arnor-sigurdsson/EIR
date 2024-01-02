07ea7ca76f5443fc7c13f96e8e85b23cbed220b1
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2024-01-02 19:32:11 +0000

    Pin torch version.

--- eir/data_load/datasets.py
+++ eir/data_load/datasets.py
@@ -393,7 +393,7 @@ def _log_missing_samples_between_modalities(
             "Please review the info log above for detailed counts and IDs. "
             "If this is expected, ignore this message. If not, possible "
             "causes are (a) different inputs having different sample IDs "
-            "present, (b) sample IDs between inputs / targets are not matching,"
+            "present, (b) sample IDs between inputs / targets are not matching, "
             "or (c) something else."
         )
         logger.warning(warning_message)
