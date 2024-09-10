Fix custom array serialization to work when no normalization is applied.

eir/experiment_io/input_object_io_modules/array_input_io.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Optional
 
 import numpy as np
 
@@ -23,10 +24,12 @@ def load_array_input_object(serialized_input_folder: Path) -> ComputedArrayInput
 
     input_config = load_input_config_from_yaml(input_config_path=config_path)
 
-    normalization_stats = load_dataclass(
-        cls=ArrayNormalizationStats,
-        file_path=normalization_stats_path,
-    )
+    normalization_stats: Optional[ArrayNormalizationStats] = None
+    if normalization_stats_path.exists():
+        normalization_stats = load_dataclass(
+            cls=ArrayNormalizationStats,
+            file_path=normalization_stats_path,
+        )
 
     dtype_str = json.loads(dtype_path.read_text())
     dtype = np.dtype(dtype_str)

eir/setup/input_setup_modules/common.py
@@ -24,7 +24,7 @@ class DataDimensions:
         return int(base * np.prod(self.extra_dims))
 
     def full_shape(self) -> Tuple[int, ...]:
-        return (self.channels, self.height, self.width) + self.extra_dims
+        return tuple([self.channels, self.height, self.width] + list(self.extra_dims))
 
 
 def get_data_dimension_from_data_source(