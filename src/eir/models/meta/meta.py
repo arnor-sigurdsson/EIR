c81779593726f03ed81e85e57fd3d2e7b5b599bd
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2025-02-27 11:03:12 -0600

    enhance(models): Add prototype scale init of transformer blocks in MetaModel.

--- src/eir/models/meta/meta.py
+++ src/eir/models/meta/meta.py
@@ -1,14 +1,19 @@
+import math
 from typing import Literal
 
 import torch
 from torch import nn
 
+from eir.models.layers.attention_layers import TransformerBlock
 from eir.models.meta.meta_utils import (
     al_fusion_modules,
     al_input_modules,
     al_output_modules,
     run_meta_forward,
 )
+from eir.utils.logging import get_logger
+
+logger = get_logger(name=__name__)
 
 
 class MetaModel(nn.Module):
@@ -28,6 +33,8 @@ class MetaModel(nn.Module):
         self.fusion_to_output_mapping = fusion_to_output_mapping
         self.tensor_broker = tensor_broker
 
+        apply_scaled_residual_init(model=self, base_std=0.02)
+
     def forward(
         self,
         inputs: dict[str, torch.Tensor],
@@ -41,3 +48,23 @@ class MetaModel(nn.Module):
         )
 
         return output_modules_out
+
+
+def apply_scaled_residual_init(model: MetaModel, base_std: float = 0.02) -> None:
+    """
+    For now, we will just apply this to TransformerBlocks, later we can look into
+    for other types of layers as well.
+    """
+    num_layers = sum(1 for m in model.modules() if isinstance(m, TransformerBlock))
+
+    scale_factor = math.sqrt(2 * num_layers)
+    scaled_std = base_std / scale_factor
+
+    logger.debug(
+        f"Applying scaled initialization "
+        f"(std={scaled_std:.6f}) for {num_layers} transformer blocks"
+    )
+
+    for name, param in model.named_parameters():
+        if any(name.endswith(suffix) for suffix in ["out_proj.weight", "w3.weight"]):
+            nn.init.normal_(param, mean=0.0, std=scaled_std)
